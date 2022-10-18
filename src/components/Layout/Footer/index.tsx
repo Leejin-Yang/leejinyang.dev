a33e4d369a4ed473e07fd6e8a2ac9f106ea506d5
Leejin-Yang <ejin1996@gmail.com>
2022-10-18 19:50:31 +0900

    feat: 폰트 최적화

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -3,7 +3,7 @@ import styles from './footer.module.scss';
 const Footer = () => {
   return (
     <footer className={styles.container}>
-      <p>© 2022 Name</p>
+      <p>© 2022 Leejin Yang</p>
     </footer>
   );
 };
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,11 @@ export default class MyDocument extends Document {
           <link rel='preconnect' href='https://fonts.googleapis.com' />
           <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='' />
           <link
-            href='https://fonts.googleapis.com/css2?family=Marcellus&family=Noto+Sans&family=Noto+Serif+KR:wght@300;400;500;600;700&display=swap'
+            href='https://fonts.googleapis.com/css2?family=Marcellus&family=Noto+Serif+KR:wght@300;400;500;600;700&display=swap'
+            rel='stylesheet'
+          />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400&display=swap&text=%C2%A9%202022%20Leejin%20Yang'
             rel='stylesheet'
           />
         </Head>
