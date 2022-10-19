06c45d68ccf6bc004b4ab3980a162d88fe6d012c
Leejin-Yang <ejin1996@gmail.com>
2022-10-19 12:50:18 +0900

    feat: 영문 폰트 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
           <link rel='preconnect' href='https://fonts.googleapis.com' />
           <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='' />
           <link
-            href='https://fonts.googleapis.com/css2?family=Marcellus&family=Noto+Serif+KR:wght@300;400;500;600;700&display=swap'
+            href='https://fonts.googleapis.com/css2?family=Noto+Serif+KR:wght@300;400;500;600;700&display=swap'
             rel='stylesheet'
           />
           <link
