914199bfe270a9a953d73b7c8d4732fd60367d46
Leejin-Yang <ejin1996@gmail.com>
2023-06-16 20:52:54 +0900

    feat: 마크다운 커스텀 분리 및 코드 다크모드 스타일 적용

--- src/components/PostContent/ContentBody/index.tsx
+++ src/components/PostContent/ContentBody/index.tsx
@@ -1,52 +1,11 @@
-/* eslint-disable @next/next/no-img-element */
 import ReactMarkdown from 'react-markdown';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { prism } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 import styles from './contentBody.module.scss';
+import customComponents from './MarkdownCustom';
 
 interface Props {
   content: string;
 }
-
-const customComponents = {
-  p(paragraph: any) {
-    const { node } = paragraph;
-
-    if (node.children[0].tagName === 'img') {
-      const image = node.children[0];
-
-      return (
-        <div className={styles.image}>
-          <img src={image.properties.src} alt={image.alt} />
-        </div>
-      );
-    }
-
-    return <p>{paragraph.children}</p>;
-  },
-
-  a(anchor: any) {
-    return (
-      <a href={anchor.href} target='_blank' rel='noopener noreferrer'>
-        {anchor.children}
-      </a>
-    );
-  },
-
-  code(code: any) {
-    const { className, children, ...props } = code;
-    const language = /language-(\w+)/.exec(className || ''); // language-tsx => tsx
-
-    return language ? (
-      <SyntaxHighlighter style={prism} language={language[1]} {...props}>
-        {String(children).replace(/\n$/, '')}
-      </SyntaxHighlighter>
-    ) : (
-      <code>{children}</code>
-    );
-  },
-};
 
 const ContentBody = ({ content }: Props) => {
   return (
