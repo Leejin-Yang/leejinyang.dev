afd104bc7df2030fd5e2e21880d4d5c591b3bed3
Leejin-Yang <ejin1996@gmail.com>
2023-06-17 16:24:36 +0900

    feat: 파비콘 적용

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,6 +16,16 @@ export default class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400&display=swap&text=%C2%A9%202022%20Leejin%20Yang'
             rel='stylesheet'
           />
+          <link rel='icon' href='/favicon.ico' />
+          <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+          <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+          <link
+            rel='apple-touch-icon'
+            type='image/png'
+            sizes='180x180'
+            href='/apple-icon-180x180.png'
+          />
+          <link rel='manifest' href='/manifest.json' />
         </Head>
         <body>
           <Main />
