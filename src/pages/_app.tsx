bb46794e5242a147ebeeed532ce0964890e6b35b
Leejin-Yang <ejin1996@gmail.com>
2023-06-17 16:46:49 +0900

    feat: 구글 애널리틱스 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,29 @@ import '@/styles/globals.scss';
 
 import ThemeProvider from 'contexts/ThemeContext';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import { SWRConfig } from 'swr';
 
 import Layout from '@/components/Layout';
+import { pageView } from '@/utils/gtag';
 
 function MyApp({ Component, pageProps }: AppProps<{ fallback: any }>) {
   const { fallback } = pageProps;
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      pageView(url);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <SWRConfig value={{ fallback }}>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,7 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
+
+import { GA_TRACKING_ID } from '@/utils/gtag';
 
 export default class MyDocument extends Document {
   render() {
@@ -26,6 +29,21 @@ export default class MyDocument extends Document {
             href='/apple-icon-180x180.png'
           />
           <link rel='manifest' href='/manifest.json' />
+          {/* Global site tag (gtag.js) - Google Analytics */}
+          <Script
+            strategy='afterInteractive'
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <Script
+            id='gtag-init'
+            strategy='afterInteractive'
+            dangerouslySetInnerHTML={{
+              __html: String.raw`
+                window.dataLayer = window.dataLayer || []; function gtag() { dataLayer.push(arguments); } gtag('js', new
+                Date()); gtag('config', '${GA_TRACKING_ID}', { page_path: window.location.pathname });
+              `,
+            }}
+          />
         </Head>
         <body>
           <Main />
